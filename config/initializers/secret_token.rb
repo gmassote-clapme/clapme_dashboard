e33f8e26c26fe1357c953d3fb83c04385ad8f479
Gabriel Massote <gabriel.massote@clapme.com.br>
2014-10-28 16:53:27 -0200

    clapme_dashboard: auto deploy

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,4 @@
-ClapmeWebsite::Application.config.secret_key_base = '7d3ec3532cc870b12af730d5631038ff900b838680f1d4b642ece3fa23bebf2327120d39960c351dbe4c8740b00c27a6a810721b5d05ca97439b570748bccdd2'
-ClapmeWebsite::Application.config.crypt_key_pre = ClapmeWebsite::Application.config.secret_key_base[0..5]
-ClapmeWebsite::Application.config.crypt_key_pos = ClapmeWebsite::Application.config.secret_key_base[10..15]
+ClapmeDashboard::Application.config.secret_key_base = '7d3ec3532cc870b12af730d5631038ff900b838680f1d4b642ece3fa23bebf2327120d39960c351dbe4c8740b00c27a6a810721b5d05ca97439b570748bccdd2'
+ClapmeDashboard::Application.config.crypt_key_pre = ClapmeWebsite::Application.config.secret_key_base[0..5]
+ClapmeDashboard::Application.config.crypt_key_pos = ClapmeWebsite::Application.config.secret_key_base[10..15]
 
