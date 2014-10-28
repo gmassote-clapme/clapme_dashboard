a7315331db09ee65560f26d342668bf393e19040
Gabriel Massote <gabriel.massote@clapme.com.br>
2014-10-28 10:32:45 -0200

    first commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,4 +16,19 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
 
+  # PaperClip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+  Paperclip.options[:content_type_mappings] = { jpeg: 'image/jpeg', jpg: 'image/jpg', jpg: 'image/jpeg', pem: 'text/plain' }
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          bucket: 'uploads.clapme.com',
+          access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
+          secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY']
+      }
+  }
+
+  # QuietAssets
+  config.quiet_assets = true
+
 end
